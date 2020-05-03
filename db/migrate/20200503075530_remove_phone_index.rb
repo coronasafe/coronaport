de5de76799a9125a3db58deab3f464f2949a244a
Bodhish Thomas <bodhish@gmail.com>
2020-05-03 13:28:22 +0530

    Remove unique index on phone number

---
+++ db/migrate/20200503075530_remove_phone_index.rb
@@ -0,0 +1,5 @@
+class RemovePhoneIndex < ActiveRecord::Migration[6.0]
+  def change
+    remove_index :contacts, :phone
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_114943) do
+ActiveRecord::Schema.define(version: 2020_05_03_075530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,7 +72,6 @@ ActiveRecord::Schema.define(version: 2020_04_27_114943) do
     t.string "test_type"
     t.index ["application_id"], name: "index_contacts_on_application_id"
     t.index ["panchayat_id"], name: "index_contacts_on_panchayat_id"
-    t.index ["phone"], name: "index_contacts_on_phone", unique: true
     t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
